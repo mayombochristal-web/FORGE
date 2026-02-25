d425795af20c98d00a9422068c7ba5f3efa12db9
mayombochristal-web <mayombochristal@gmail.com>
2026-02-25 23:02:56 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 from utils import download_corpus
+import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str, default='shakespeare',
-                        choices=['shakespeare', 'wikitext', 'tiny_shakespeare'])
+    parser.add_argument('--name', type=str, default='shakespeare')
     args = parser.parse_args()
     path = download_corpus(args.name)
     print(f"Corpus téléchargé : {path}")
