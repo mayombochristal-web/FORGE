5ebc13b1064fd36392160505e1616cfca76247b9
mayombochristal-web <mayombochristal@gmail.com>
2026-02-25 21:42:59 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,12 +1,8 @@
-# Dans engine.py, modifiez la fonction process_signal
 def process_signal(self, impulse):
     solver = ode(self.system).set_integrator('vode', method='bdf')
     solver.set_initial_value(self.state, 0).set_f_params(impulse)
     self.state = solver.integrate(0.1)
     
-    # BRIDAGE DE SÉCURITÉ (Évite les nombres géants)
-    for i in range(3):
-        if self.state[i] > 100: self.state[i] = 100 # Plafond à 100
-        if self.state[i] < -100: self.state[i] = -100 # Plancher à -100
-        
+    # Écrêtage Triadique : On maintient les variables dans le monde réel
+    self.state = [np.clip(s, -100, 100) for s in self.state]
     return self.state
