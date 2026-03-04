27467d9f064cfe5186ca18c514ca525ea7049149
kiri Séchéta <mayombochristal@gmail.com>
2026-03-04 22:05:02 +0100

    Delete train_tokenizer.py

--- train_tokenizer.py
+++
@@ -1,7 +0,0 @@
-tokenizer = spm.SentencePieceProcessor()
-tokenizer.Load("tokenizer/tokenizer.model")
-vocab_size = tokenizer.get_piece_size()
-
-model = OmegaTTU_LLM(vocab_size=vocab_size)
-state_dict = torch.load("model_weights/omega_ttu_llm.pt", map_location=device)
-model.load_state_dict(state_dict)
