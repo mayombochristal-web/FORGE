7ae9fba41e83d01ef36a7ccc357c1d2ffb74c57d
kiri Séchéta <mayombochristal@gmail.com>
2026-03-07 21:38:36 +0100

    Delete analytics_engine.py

--- analytics_engine.py
+++
@@ -1,8 +0,0 @@
-class AnalyticsEngine:
-    def analyze(self, text):
-        words = text.split()
-        return {
-            "chars": len(text),
-            "words": len(words),
-            "unique_words": len(set(words))
-        }
