55c5911a0568f0041c3115e904e61e74ae388204
kiri Séchéta <mayombochristal@gmail.com>
2026-03-07 21:21:39 +0100

    Update github_memory.py

--- github_memory.py
+++ github_memory.py
@@ -1,8 +1,6 @@
 import subprocess
-import os
 
 def push_memory():
-    """Ajoute, commit et push le dossier oracle_memory sur GitHub"""
     try:
         subprocess.run(["git", "add", "oracle_memory/"], check=True)
         subprocess.run(["git", "commit", "-m", "ORACLE memory update"], check=True)
