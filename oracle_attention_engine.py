61f6b3161b21d25068f7af494be1048f3fe73b02
kiri Séchéta <mayombochristal@gmail.com>
2026-03-07 21:01:28 +0100

    Update oracle_attention_engine.py

--- oracle_attention_engine.py
+++ oracle_attention_engine.py
@@ -1,11 +1,6 @@
 class AttentionEngine:
-
-    def score(self,tokens):
-
-        unique=len(set(tokens))
-
-        length=len(tokens)
-
-        score=(unique*0.5)+(length*0.2)
-
+    def score(self, tokens):
+        unique = len(set(tokens))
+        length = len(tokens)
+        score = (unique * 0.5) + (length * 0.2)
         return score
