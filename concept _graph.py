c9ec36d781b508d9a203c124e6da43e30bee3491
kiri Séchéta <mayombochristal@gmail.com>
2026-03-07 20:41:54 +0100

    Create concept _graph.py

---
+++ concept _graph.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class ConceptGraph:
+
+    def __init__(self):
+
+        self.graph=Counter()
+
+    def update(self,tokens):
+
+        for t in tokens:
+
+            self.graph[t]+=1
+
+    def top(self,n=20):
+
+        return self.graph.most_common(n)
